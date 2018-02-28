eb4e99c7cb31cbaaa54990bb06116e93d5d64568
Administrator <Administrator@10.0.2.15>
2018-02-27 20:28:52 -0800

    fixed the code corresponding to 1223

---
+++ selenuimbasics/src/demo/Verfytitle.java
@@ -0,0 +1,24 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Verfytitle {
+	
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+		String expectedTitle = "Facebook - Log In or Sign Up";
+		String acutualtitle = driver.getTitle();//getTile is a predefined fun which we get the title of the page which we are working on
+		if(expectedTitle.equals(acutualtitle)){
+			System.out.println("Test Case Passed");
+		}
+		else {
+			System.out.println("Test Case Failed");
+			
+		}
+	
+	}
+}
---
+++ selenuimbasics/src/demo/verifyerrormessage.java
@@ -0,0 +1,19 @@
+package demo;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class verifyerrormessage {
+	public static void main(String[] args) {
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://facebook.com");
+		driver.findElement(By.id("email")).sendKeys("batman@gmail.com");
+		driver.findElement(By.id("pass")).sendKeys("password");
+		driver.findElement(By.id("loginbutton")).click();
+	}
+
+
+}
